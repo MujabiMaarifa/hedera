99fbd4973a73a760a1a9532424b0d53ad544931b
MujabiMaarifa <daudimujabi@gmail.com>
2025-11-14 10:36:25 -0500

    updates on the nfts

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const { Client, PrivateKey, AccountCreateTransaction, AccountBalanceQuery, Hbar, TransferTransaction } = require("@hashgraph/sdk");
+const { Client,
+	PrivateKey,
+	AccountCreateTransaction,
+	AccountBalanceQuery,
+	Hbar, 
+	TransferTransaction,
+	TokenCreateTransaction,
+	TokenType,
+	TokenSupplyType,
+	TokenAssociateTransaction,
+} = require("@hashgraph/sdk");
+
 require('dotenv').config();
 
 async function environmentSetup() {
@@ -36,7 +47,7 @@ async function environmentSetup() {
 	const accountBalance = await new AccountBalanceQuery()
 		.setAccountId(newAccountId)
 		.execute(client);
-	console.log("The new account balance is: " + accountBalance.hbars.toTinybars() + " tinybar .");
+	console.log("The new account balance is: " + accountBalance.hbars.toTinybars() + " tinybars.");
 
 	//send some hbar, since we are transferring from the client to new account there is no need of signing the transactio
 	const sendHbar = await new TransferTransaction()
@@ -44,9 +55,73 @@ async function environmentSetup() {
 		.addHbarTransfer(newAccountId, Hbar.fromTinybars(1000))
 		.execute(client);
 	
-	//verify the transaction reached concensus
+	//verify the transaction reached consensus
 	const transactionReceipt = await sendHbar.getReceipt(client);
 	console.log("The transfer transaction from my account to the new account is: " + transactionReceipt.status.toString());
+
+	const supplyKey = PrivateKey.generate();
+
+	//create tokens -- Tokens are assets and have the monetary value
+	//fungible tokens -> they can be swapped, they have equal values and are identical
+	let tokenCreate = await new TokenCreateTransaction()
+		.setTokenName("Maarifa")
+		.setTokenSymbol("Maa")
+		.setTokenType(TokenType.FungibleCommon)
+		.setDecimals(2)
+		.setInitialSupply(10000)
+		.setTreasuryAccountId(accountId)
+		.setSupplyType(TokenSupplyType.Infinite)
+		.setSupplyKey(supplyKey)
+		.freezeWith(client);
+	//sign with treasury key
+	let tokenCreateSign = await tokenCreate.sign(privateKey);
+
+	//submit the transaction
+	let tokenCreateSubmit = await tokenCreateSign.execute(client);
+
+	//get the transaction receipt
+	let tokenCreateRx = await tokenCreateSubmit.getReceipt(client);
+
+	//get the token id
+	let tokenId = tokenCreateRx.tokenId;
+
+	console.log(`- created token with id: ${tokenId} \n`);
+
+	//create hedera token association transaction to send the nfts 
+	const transactionAssociation = await new TokenAssociateTransaction()
+		.setAccountId(newAccountId)
+		.setTokenIds([tokenId])
+		.freezeWith(client);
+	
+	//sign the transaction with the newly created account
+	const signTx = await transactionAssociation.sign(newAccountPrivateKey);
+
+	const txResponse = await signTx.execute(client);
+
+	//get the associate receipt
+	const associateReceipt = await txResponse.getReceipt(client);
+
+	const associateStatus = await associateReceipt.status;
+
+	console.log("The transaction associate status was: " + associateStatus);
+
+	//set the balance check here initial account balance
+	const transferTransaction = await new TransferTransaction()
+		.addTokenTransfer(tokenId, accountId, -10)
+		.addTokenTransfer(tokenId, newAccountId, 10)
+		.freezeWith(client)
+
+	const signTransferTx = await transferTransaction.sign(privateKey);
+
+	const transferTxResponse = await signTransferTx.execute(client);
+
+	const transferTxReceipt = await transferTxResponse.getReceipt(client);
+
+	const transferTxStatus = await transferTxReceipt.status
+
+	console.log("The transfer status is: " + transferTxStatus)
+
+	//check the balance after transfer
 }
 
 environmentSetup();
